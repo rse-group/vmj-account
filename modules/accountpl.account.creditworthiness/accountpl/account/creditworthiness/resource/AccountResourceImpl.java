7119a0ccb64099686e98ed39be9bb93b5393c7b7
Will S <williamsusanto82@gmail.com>
2025-06-13 19:14:19 +0700

    [FEAT] Fix all broken features including dailylimit and creditworthiness

--- modules/accountpl.account.creditworthiness/accountpl/account/creditworthiness/resource/AccountResourceImpl.java
+++ modules/accountpl.account.creditworthiness/accountpl/account/creditworthiness/resource/AccountResourceImpl.java
@@ -3,10 +3,18 @@ package accountpl.account.creditworthiness;
 import accountpl.account.core.AccountResourceDecorator;
 import accountpl.account.core.AccountImpl;
 import accountpl.account.core.AccountResourceComponent;
+import accountpl.account.AccountFactory;
+import accountpl.account.core.Account;
+
+import java.util.*;
+
+import vmj.routing.route.Route;
+import vmj.routing.route.VMJExchange;
+import prices.auth.vmj.annotations.Restricted;
 
 public class AccountResourceImpl extends AccountResourceDecorator {
     public AccountResourceImpl (AccountResourceComponent record) {
-        // to do implement the method
+        super(record);
     }
 
     // @Restriced(permission = "")
@@ -15,46 +23,47 @@ public class AccountResourceImpl extends AccountResourceDecorator {
 		if (vmjExchange.getHttpMethod().equals("OPTIONS")) {
 			return null;
 		}
-		  = create(vmjExchange);
-		Repository.saveObject();
+		Account account = create(vmjExchange);
+		accountRepository.saveObject(account);
 		return getAll(vmjExchange);
 	}
 
-    public  create(VMJExchange vmjExchange){
-		AccountImpl account = (AccountImpl) vmjExchange.getRequestBodyForm("account);
-		
-		  = record.create(vmjExchange);
-		 deco = Factory.create("accountpl.creditworthiness.core.AccountImpl", );
-			return deco;
+    public Account create(VMJExchange vmjExchange){
+    	String interest_rateStr = (String) vmjExchange.getRequestBodyForm("interest_rate");
+		double interest_rate = Double.parseDouble(interest_rateStr);
+		Account account = record.createAccount(vmjExchange);
+		Account deco = AccountFactory.createAccount("accountpl.creditworthiness.core.AccountImpl", account, interest_rate);
+		return deco;
 	}
 
     // @Restriced(permission = "")
     @Route(url="call/creditworthiness/update")
     public HashMap<String, Object> update(VMJExchange vmjExchange){
 		// to do implement the method
+    	return new HashMap<>();
 	}
 
 	// @Restriced(permission = "")
     @Route(url="call/creditworthiness/detail")
     public HashMap<String, Object> get(VMJExchange vmjExchange){
-		return record.get(vmjExchange);
+		return record.getAccount(vmjExchange);
 	}
 
 	// @Restriced(permission = "")
     @Route(url="call/creditworthiness/list")
     public List<HashMap<String,Object>> getAll(VMJExchange vmjExchange){
-		List<> List = Repository.getAllObject("_impl");
-		return transformListToHashMap(List);
+		List<Account> accountList = accountRepository.getAllObject("_impl");
+		return transformAccountListToHashMap(accountList);
 	}
 
-    public List<HashMap<String,Object>> transformListToHashMap(List<> List){
-		List<HashMap<String,Object>> resultList = new ArrayList<HashMap<String,Object>>();
-        for(int i = 0; i < List.size(); i++) {
-            resultList.add(List.get(i).toHashMap());
-        }
-
-        return resultList;
-	}
+//    public List<HashMap<String,Object>> transformListToHashMap(List<> List){
+//		List<HashMap<String,Object>> resultList = new ArrayList<HashMap<String,Object>>();
+//        for(int i = 0; i < List.size(); i++) {
+//            resultList.add(List.get(i).toHashMap());
+//        }
+//
+//        return resultList;
+//	}
 
 	// @Restriced(permission = "")
     @Route(url="call/creditworthiness/delete")
@@ -63,14 +72,16 @@ public class AccountResourceImpl extends AccountResourceDecorator {
 			return null;
 		}
 		
-		String idStr = (String) vmjExchange.getRequestBodyForm("");
+		String idStr = (String) vmjExchange.getRequestBodyForm("id_account");
 		int id = Integer.parseInt(idStr);
-		Repository.deleteObject(id);
+		accountRepository.deleteObject(id);
 		return getAll(vmjExchange);
 	}
 
 	protected boolean credit(int amount) {
 		// TODO: implement this method
+		
+		return true;
 	}
 }
 
--- modules/accountpl.account.dailylimit/accountpl/account/dailylimit/model/AccountImpl.java
+++ modules/accountpl.account.dailylimit/accountpl/account/dailylimit/model/AccountImpl.java
@@ -3,15 +3,19 @@ package accountpl.account.dailylimit;
 import accountpl.account.core.AccountDecorator;
 import accountpl.account.core.Account;
 import accountpl.account.core.AccountComponent;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Column;
 
+
+@Entity(name="account_dailylimit")
+@Table(name="account_dailylimit")
 public class AccountImpl extends AccountDecorator {
 
 	protected int dailyLimit;
 	protected int withdraw;
-	public AccountImpl(AccountComponent record, int dailyLimit, int withdraw) {
-		super(record);
-		this.dailyLimit = dailyLimit;
-		this.withdraw = withdraw;
+	public AccountImpl() {
+		super();
 	}
 
 	public int getDailyLimit() {
